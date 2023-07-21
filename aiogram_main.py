0223f7c373b055bf946e451e044d73ffb0b3a4b8
Artur-1995 <89254423508@mail.ru>
2023-07-21 22:27:47 +0300

    Ubdate 21/07/2023

--- aiogram_main.py
+++ aiogram_main.py
@@ -1,14 +1,50 @@
-import buttons
-from Token import API_TOKEN
+# from Tokens import API_TOKEN, OWM_TOKEN
 from aiogram import Bot, types, Dispatcher, executor
+from pyowm import OWM
+import random
+import os
 
-
-bot = Bot(API_TOKEN)
+excepshon_answer = ['Мда, давай еще разок по внимательнее', "Все обыскал, нет блин такого города!\nПопробуй еще раз..."]
+owm = OWM(os.getenv('OWM_TOKEN'))
+bot = Bot(os.getenv('API_TOKEN'))
 dp = Dispatcher(bot)
-
+mgr = owm.weather_manager()
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.answer("Привет!\nНапиши мне что-нибудь!")
+    # await message.answer('Привет, {0.first_name}!'.format(message.from_user)+
+    #                  '\nЯ могу сказать какая погода в твоем городе')
+    await message.answer('Привет, {0.first_name}!'.format(message.from_user)+
+                     '\nЯ могу сказать какая погода в твоем городе')
+@dp.message_handler(types.Message)
+async def echo_message(message: types.Message):
+    # await message.delete()
+    try:
+        clouds = {'scattered clouds': 'Ясно',
+                  'overcast clouds': 'Облачно',
+                  'few clouds': 'Небольшая облачность',
+                  'broken clouds': 'Облачно с прояснениями',
+                  'clear sky': 'Чистое небо',
+                  'moderate rain': 'Умеренный дождь'}
+        observation = mgr.weather_at_place(message.text)
+        w = observation.weather
+        msg = str(message.text).upper()[0] + str(message.text).lower()[1:]
+
+
+        await message.answer((f'Погода в городе {msg}' +
+                               '\n' + "Температура " + str(int(w.temperature('celsius')['temp'])) +
+                               '\n' + f"Скорость ветра {int(w.wind()['speed'])} м/с" +
+                               '\n' + (clouds[w.detailed_status] if w.detailed_status in clouds.keys() else '') +
+                               '\n' + 'Посмотреть погоду в другом городе?'))
+    # except:
+    #     await bot.send_message(message.chat.id, 'Ошибка! Город не найден.')
+    except:
+        await bot.send_message(message.chat.id, random.choice(excepshon_answer))
+
+
+# input()
+
+
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
