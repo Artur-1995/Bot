15ca8ad17dc49b097e80904592dea1d0ec8838f6
Artur-1995 <89254423508@mail.ru>
2023-07-31 20:20:47 +0300

    ubdate butons.py 31/07/2023

--- buttons.py
+++ buttons.py
@@ -1,25 +1,31 @@
 import aiogram
-import telebot
+from telebot import types
 kb_search_aiogram = aiogram.types.InlineKeyboardMarkup(row_width=1)
-btn1 = aiogram.types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
-btn2 = aiogram.types.InlineKeyboardButton(text='Определить город', callback_data='get_geo')
-kb_search_aiogram.add(btn1, btn2)
+btn_input_city_name = types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
+btn_get_geo = types.InlineKeyboardButton(text='Определить город', callback_data='get_geo')
+kb_search_aiogram.add(btn_input_city_name, btn_get_geo)
 
-kb_search_telebot = telebot.types.InlineKeyboardMarkup(row_width=1)
-btn_input_city_name = telebot.types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
-btn_get_geo = telebot.types.InlineKeyboardButton(text='Определить город', callback_data='get_geo')
+btn_input_city_name = types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
+btn_get_geo = types.InlineKeyboardButton(text='Определить город', callback_data='get_geo')
+btn_yes = types.InlineKeyboardButton(text='Да', callback_data='input_city_name')
+btn_ask_input_city_name = types.InlineKeyboardButton(text='Ввести в ручную?', callback_data='input_city_name')
+
+
+kb_search_telebot = types.InlineKeyboardMarkup(row_width=1)
+btn_input_city_name
+btn_get_geo
 kb_search_telebot.add(btn_input_city_name, btn_get_geo)
 
-keyboard_continuation = telebot.types.InlineKeyboardMarkup(row_width=1)
-btn_yes = telebot.types.InlineKeyboardButton(text='Да', callback_data='input_city_name')
+keyboard_continuation = types.InlineKeyboardMarkup(row_width=1)
+btn_yes
 keyboard_continuation.add(btn_yes)
 
-keyboard_back = telebot.types.InlineKeyboardMarkup(row_width=1)
-btn_ask_input_city_name = telebot.types.InlineKeyboardButton(text='Ввести в ручную?', callback_data='input_city_name')
+keyboard_back = types.InlineKeyboardMarkup(row_width=1)
+btn_ask_input_city_name
 keyboard_back.add(btn_ask_input_city_name)
 
-kb_search_telebot_short = telebot.types.InlineKeyboardMarkup(row_width=1)
-btn_input_city_name = telebot.types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
+kb_search_telebot_short = types.InlineKeyboardMarkup(row_width=1)
+btn_input_city_name
 kb_search_telebot_short.add(btn_input_city_name)
 
 
