4fd5c20618f59b91fd3ad011dd7f3f914feac49e
Artur-1995 <89254423508@mail.ru>
2023-07-31 19:21:21 +0300

    ubdate butons.py 31/07/2023

--- buttons.py
+++ buttons.py
@@ -6,19 +6,20 @@ btn2 = aiogram.types.InlineKeyboardButton(text='Определить город'
 kb_search_aiogram.add(btn1, btn2)
 
 kb_search_telebot = telebot.types.InlineKeyboardMarkup(row_width=1)
-btn1 = telebot.types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
-btn2 = telebot.types.InlineKeyboardButton(text='Определить город', callback_data='get_geo')
-kb_search_telebot.add(btn1, btn2)
+btn_input_city_name = telebot.types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
+btn_get_geo = telebot.types.InlineKeyboardButton(text='Определить город', callback_data='get_geo')
+kb_search_telebot.add(btn_input_city_name, btn_get_geo)
 
 keyboard_continuation = telebot.types.InlineKeyboardMarkup(row_width=1)
-btn1 = telebot.types.InlineKeyboardButton(text='Да', callback_data='input_city_name')
-keyboard_continuation.add(btn1)
+btn_yes = telebot.types.InlineKeyboardButton(text='Да', callback_data='input_city_name')
+keyboard_continuation.add(btn_yes)
 
 keyboard_back = telebot.types.InlineKeyboardMarkup(row_width=1)
-btn1 = telebot.types.InlineKeyboardButton(text='Ввести в ручную?', callback_data='input_city_name')
-keyboard_back.add(btn1)
+btn_ask_input_city_name = telebot.types.InlineKeyboardButton(text='Ввести в ручную?', callback_data='input_city_name')
+keyboard_back.add(btn_ask_input_city_name)
 
 kb_search_telebot_short = telebot.types.InlineKeyboardMarkup(row_width=1)
-btn1 = telebot.types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
-kb_search_telebot_short.add(btn1)
+btn_input_city_name = telebot.types.InlineKeyboardButton(text='Ввести название города', callback_data='input_city_name')
+kb_search_telebot_short.add(btn_input_city_name)
+
 
