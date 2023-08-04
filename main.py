58647e71198e2a06a255f46066a8cf9a998d743b
Artur-1995 <89254423508@mail.ru>
2023-08-05 00:48:18 +0300

    updated file 'main.py' 04/08/2023

--- main.py
+++ main.py
@@ -14,30 +14,36 @@ owm = OWM(OWM_TOKEN)
 bot = telebot.TeleBot(API_TOKEN)
 mgr = owm.weather_manager()
 
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(func=lambda message: True)
+# @bot.message_handler(commands=['start'])
 def start_command(message):
-    global user_name
-    user_name = '{0.first_name}'.format(message.from_user)
+    user_name = message.from_user.first_name
 
-    bot.send_message(message.chat.id, text=f'Привет, {str(user_name)}!' +
-                '\nЯ могу сказать какая погода в твоем городе!', reply_markup=buttons.kb_search_telebot)
-    print(f'Пользователя \'{str(user_name)}\' запустил бота!')
+    if message.text == '/start':
+        bot.send_message(message.chat.id, text=f'Привет, {user_name}!' +
+                    '\nЯ могу сказать какая погода в твоем городе!', reply_markup=buttons.kb_search_telebot)
+        print(f'Пользователя \'{user_name}\' запустил бота!')
+    if message.text != '/start':
+        bot.send_message(message.chat.id, 'Сначала нужно выбрать вариант из меню',
+                         reply_markup=buttons.kb_search_telebot)
+        print(
+            f'Пользователь \'{message.from_user.first_name}\' ввел \"{message.text}\" и обошел шаблон взаимодействия')
 
 @bot.callback_query_handler(func=lambda callback: callback.data)
 def check_callback_data(callback):
 
-    global user_name
+    user_name = callback.from_user.first_name
 
     if callback.data == 'get_geo':
-        bot.send_message(callback.message.chat.id, f'{str(user_name)} эта функция пока что для тебя недоступна!',
+        bot.send_message(callback.message.chat.id, f'{user_name} эта функция пока что для тебя недоступна!',
                          reply_markup=buttons.keyboard_back)
-        print(f'Поиск города по геолокации для пользователя \'{str(user_name)}\' недоступно!')
+        print(f'Поиск города по геолокации для пользователя \'{user_name}\' недоступно!')
     if callback.data == 'input_city_name':
-        bot.send_message(callback.message.chat.id, f'{str(user_name)} напиши название города')
-        print(f'Ожидание ввода от пользователя \'{str(user_name)}\'')
+        bot.send_message(callback.message.chat.id, f'{user_name} напиши название города')
+        print(f'Ожидание ввода от пользователя \'{user_name}\'')
 
         @bot.message_handler(func=lambda message: True)
+
         def search_wether(message):
             try:
                 clouds = {'scattered clouds': 'Ясно',
@@ -56,18 +62,12 @@ def check_callback_data(callback):
                                            clouds[w.detailed_status] if w.detailed_status in clouds.keys() else '')))
 
                 bot.send_message(message.chat.id, text='Посмотреть погоду в другом городе?', reply_markup=buttons.keyboard_continuation)
-                print(f'Пользователь \'{str(user_name)}\' искал погоду в городе \'{message.text}\'')
+                print(f'Пользователь \'{user_name}\' искал погоду в городе \'{message.text}\'')
 
             except:
                 bot.send_message(message.chat.id, 'Ошибка! Город не найден.', reply_markup=buttons.kb_search_telebot_short)
-                print(f'Пользователь \'{str(user_name)}\' ввел нераспознанное названия города \"{message.text}\"')
+                print(f'Пользователь \'{user_name}\' ввел нераспознанное названия города \"{message.text}\"')
 
-        # except:
-        #     @bot.message_handler(func=lambda message: True)
-        #     def less_shablon(message):
-        #         bot.send_message(message.chat.id, 'Сначала нужно выбрать вариант из меню',
-        #                          reply_markup=buttons.kb_search_telebot)
-        #         print(f'Пользователь \'{str(user_name)}\' ввел \"{message.text}\" и обошел шаблон взаимодействия')
 
 if __name__ == "__main__":
     bot.polling()
